c2dc990ae3edb1b77f84e0cf36c2fdc32076e063
PauloAlves8039 <pj38alves@gmail.com>
2024-12-07 14:28:54 -0300

    feat: adicionados estilos iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css"
 
 function App() {
   
   return (
-    <div></div>
+    <div className="App">
+      <h1>TESTE</h1>
+    </div>
   )
 }
 
