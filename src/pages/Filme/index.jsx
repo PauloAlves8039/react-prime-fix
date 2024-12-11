6d6f24d38871d73344c5a518956f83422a1fb809
PauloAlves8039 <pj38alves@gmail.com>
2024-12-11 11:28:49 -0300

    feat: implementado estilo para detalhes do filme

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
-
-import api from '../../services/api';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./filme-info.css";
+import api from "../../services/api";
 
 function Filme(){
   const { id } = useParams();
@@ -47,6 +47,16 @@ function Filme(){
       <span>{filme.overview}</span>
 
       <strong>Avalição: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+      <button>Salvar</button>
+      <button>
+        <a href="#">
+        Trailer
+        </a>
+      </button>
+      
+      </div>
 
     </div>
   )
